df5834326a1fbb81fe74527b48ec8cf9a9a13e50
abtirish <abtirish@gmail.com>
2023-10-14 10:32:58 -0600

    finish phase 1 scope lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+
+// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+console.log(customerName);
+
+
+
+
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob' 
+
+    return bestCustomer
+    }
+
+console.log(setBestCustomer)
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+
+    return bestCustomer
+}
+
+console.log(overwriteBestCustomer)
+
+const leastFavoriteCustomer = 'Amanda'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Allan'
+
+    return leastFavoriteCustomer
+};
+
+console.log(leastFavoriteCustomer);
